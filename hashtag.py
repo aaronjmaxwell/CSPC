42f028b61d29fa39965a86231ffa46a5ff83a62a
Aaron J. Maxwell, PhDI only answer queries on the fifth Wednesday of each month <ajmax.85@gmail.com>
2018-02-14 14:15:55 -0500

    Beginning Tracking of CSPC2018
    Now includes a way to track whether a tweet is original, or artificially
    generated by the user's Twitter App when in reality it was a retweet.
    This is done by checking whether the characters 'RT @' appear in the
    text of the tweet.

--- hashtag.py
+++ hashtag.py
@@ -9,7 +9,7 @@ auth = tweepy.OAuthHandler(cf['AUTH']['CONSUMER_KEY'], cf['AUTH']['CONSUMER_SECR
 auth.set_access_token(cf['AUTH']['OAUTH_TOKEN'], cf['AUTH']['OAUTH_TOKEN_SECRET'])
 # launch api
 api = tweepy.API(auth)
-ht = 'CSPC2017'
+ht = 'CSPC2018'
 f = 'stream.out'
 tweet = tweepy.Cursor(api.search, q = ht, since = cf['DATE']['s'], until = cf['DATE']['u']).items()
 out = open(f, "w")
@@ -19,10 +19,15 @@ for i,t in enumerate(tweet):
             if (j % 60 == 0):
                 print(j)
             time.sleep(1)
+    rt = t.text.find('RT @')
     string = t.created_at
     string = string.strftime('%x,%r')
     string = string + ',' + t.author.screen_name
     string = string + ',' + str(t.favorite_count) + ',' + str(t.retweet_count)
-    string = string + '\n'
+    if (rt < 0):
+        string = string + ',True,\n'
+    else:
+        txt = t.text[(rt + 4):t.text.find(':')]
+        string = string + ',False,' + txt + '\n'
     out.write(string)
 out.close()
