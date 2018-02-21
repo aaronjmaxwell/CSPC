9c242dbd23114c425d6a4c2444e923a1f2d61534
Aaron J. Maxwell, PhDI only answer queries on the fifth Wednesday of each month <ajmax.85@gmail.com>
2018-02-21 16:59:35 -0500

    Up to Feb 20

--- stream.sh
+++ stream.sh
@@ -4,7 +4,7 @@ awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' stream.out | sed 's/\//
 #python3 promotion.py
 #awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' panelstream.out | sed 's/\//,/g' | sed     's/:/,/g' | sed 's/ /,/g' | sed "s/(/'(/g" | sed "s/,)/)/g" | sed "s/)/)'/g" >> promotion.csv
 
-python3 cleaner.py tweet_activity_metrics_sciencepolicy___en.csv
+python3 cleaner.py tweet_activity_metrics_sciencepolicy_20180214_20180221_en.csv
 awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' activity.out >> activity.csv
 
 rm -fv *.out
