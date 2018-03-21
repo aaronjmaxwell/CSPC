1ca4eff74ef0d47fa36e6bd909a054f4ee9508f1
Aaron J. Maxwell, PhDI only answer queries on the fifth Wednesday of each month <ajmax.85@gmail.com>
2018-03-21 13:03:34 -0400

    Up-to-date.  stream now explictly bash. var for activity file.

--- stream.sh
+++ stream.sh
@@ -1,13 +1,16 @@
+#!/bin/bash
+file=tweet_activity_metrics_sciencepolicy_20180314_20180321_en.csv
+
 python3 hashtag.py
 awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' stream.out | sed 's/\//,/g' | sed 's/:/,/g' | sed 's/ /,/g' >> hashtag.csv
 
 #python3 promotion.py
 #awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' panelstream.out | sed 's/\//,/g' | sed     's/:/,/g' | sed 's/ /,/g' | sed "s/(/'(/g" | sed "s/,)/)/g" | sed "s/)/)'/g" >> promotion.csv
 
-python3 cleaner.py tweet_activity_metrics_sciencepolicy_20180307_20180314_en.csv
+python3 cleaner.py $file
 awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' activity.out >> activity.csv
 
-rm -fv *.out tweet_activity_metrics_sciencepolicy_20180307_20180314_en.csv
+rm -fv *.out $file
 python3 plot_hashtag.py
 python3 plot_activity.py
 #python3 mentions.py
