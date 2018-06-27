4db5164b8f0d9432ac1c483845ce3074841ab41e
Aaron J. Maxwell, PhD - I only answer queries on the fifth Wednesday of each month <ajmax.85@gmail.com>
2018-06-27 15:22:42 -0400

    Up-to-date

--- promotion.py
+++ promotion.py
@@ -10,7 +10,7 @@ auth.set_access_token(cf['AUTH']['OAUTH_TOKEN'], cf['AUTH']['OAUTH_TOKEN_SECRET'
 # launch api
 api = tweepy.API(auth)
 tweet = tweepy.Cursor(api.search, q = cf['INFO']['hashtag'], since = cf['DATE']['since'], until
-    = cf['DATE']['until']).items()
+    = cf['DATE']['until'], tweet_mode = "extended").items()
 out = open(cf['INFO']['file_'], "w")
 for i, t in enumerate(tweet):
     #if ((i + 1) % 1000 == 0):
@@ -18,14 +18,16 @@ for i, t in enumerate(tweet):
     #        if (j % 60 == 0):
     #            print(j)
     #        time.sleep(1)
+    #a = api.get_status(t.id, tweet_mode = 'extended')
     if ((t.author.screen_name == 'sciencepolicy') & (len(t.entities['user_mentions']) > 0) &
-        (t.text.find('RT @') < 0)):
-        print(t.text)
+        (t.full_text.find('RT @') < 0)):
+
         string = t.created_at
         string = string.strftime('%x,%r')
         string = string + ',' + str(t.favorite_count) + ',' + str(t.retweet_count)
-        for handle in t.entities['user_mentions']:
-            string = string + ',' + handle['screen_name']
+        handle = {key['id_str']: key['screen_name'] for key in t.entities['user_mentions']}
+        for screen_name in handle.values():
+            string = string + ',' + screen_name
         string = string + '\n'
         out.write(string)
 out.close()
