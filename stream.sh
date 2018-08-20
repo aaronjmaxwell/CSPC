17c14cd8796b0c4a87fe54f10446946e25e870e4
Aaron J. Maxwell, PhD - I only answer queries on the fifth Wednesday of each month <ajmax.85@gmail.com>
2018-08-20 16:15:49 -0400

    >.<

--- stream.sh
+++ stream.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 file=tweet_activity_metrics_sciencepolicy_20180808_20180815_en.csv
 
-python3 hashtag.py
+python3 code/hashtag.py
 awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' stream.out | sed 's/\//,/g' | sed 's/:/,/g' | sed 's/ /,/g' >> data/hashtag.csv
 
-python3 promotion.py
+python3 code/promotion.py
 awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' panelstream.out | sed 's/\//,/g' | sed     's/:/,/g' | sed 's/ /,/g' | sed "s/(/'(/g" | sed "s/,)/)/g" | sed "s/)/)'/g" >> data/promotion.csv
 
-python3 cleaner.py $file
+python3 code/sciparl.py
+awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' smpstream.out | sed 's/\//,/g' | sed     's/:/,/g' | sed 's/ /,/g' | sed "s/(/'(/g" | sed "s/,)/)/g" | sed "s/)/)'/g" >> data/sciparl.csv
+
+python3 code/cleaner.py $file
 awk '{a[i++]=$0} END {for (j=i-1; j>=0;) print a[j--] }' activity.out >> data/activity.csv
 
 rm -fv *.out $file
