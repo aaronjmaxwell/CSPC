7780afee0af6d439e4ffb2b790e8b8861211a803
Aaron J. Maxwell, PhD - I only answer queries on the fifth Wednesday of each month <ajmax.85@gmail.com>
2018-10-03 12:22:26 -0400

    Added Error checking

--- code/hashtag.py
+++ code/hashtag.py
@@ -12,21 +12,24 @@ api = tweepy.API(auth)
 tweet = tweepy.Cursor(api.search, q = cf['CONF']['hashtag'], since = cf['DATE']['since'], until
     = cf['DATE']['until']).items()
 out = open(cf['CONF']['file'], "w")
-for i, t in enumerate(tweet):
-    if ((i + 1) % 1000 == 0):
-        for j in range(600):
-            if (j % 60 == 0):
-                print(j)
-            time.sleep(1)
-    rt = t.text.find('RT @')
-    string = t.created_at
-    string = string.strftime('%x,%r')
-    string = string + ',' + t.author.screen_name
-    string = string + ',' + str(t.favorite_count) + ',' + str(t.retweet_count)
-    if (rt < 0):
-        string = string + ',True,\n'
-    else:
-        txt = t.text[(rt + 4):t.text.find(':')]
-        string = string + ',False,' + txt + '\n'
-    out.write(string)
+try:
+    for i, t in enumerate(tweet):
+        if ((i + 1) % 1000 == 0):
+            for j in range(600):
+                if (j % 60 == 0):
+                    print(j)
+                time.sleep(1)
+        rt = t.text.find('RT @')
+        string = t.created_at
+        string = string.strftime('%x,%r')
+        string = string + ',' + t.author.screen_name
+        string = string + ',' + str(t.favorite_count) + ',' + str(t.retweet_count)
+        if (rt < 0):
+            string = string + ',True,\n'
+        else:
+            txt = t.text[(rt + 4):t.text.find(':')]
+            string = string + ',False,' + txt + '\n'
+        out.write(string)
+except Exception as error:
+    print(__name__, error)
 out.close()
--- code/promotion.py
+++ code/promotion.py
@@ -12,22 +12,25 @@ api = tweepy.API(auth)
 tweet = tweepy.Cursor(api.search, q = cf['PANEL']['hashtag'], since = cf['DATE']['since'], until
     = cf['DATE']['until'], tweet_mode = "extended").items()
 out = open(cf['PANEL']['file'], "w")
-for i, t in enumerate(tweet):
-    if ((i + 1) % 1000 == 0):
-        for j in range(600):
-            if (j % 60 == 0):
-                print(j)
-            time.sleep(1)
+try:
+    for i, t in enumerate(tweet):
+        if ((i + 1) % 1000 == 0):
+            for j in range(600):
+                if (j % 60 == 0):
+                    print(j)
+                time.sleep(1)
 
-    if ((t.author.screen_name == 'sciencepolicy') & (len(t.entities['user_mentions']) > 0) &
+        if ((t.author.screen_name == 'sciencepolicy') & (len(t.entities['user_mentions']) > 0) &
         (t.full_text.find('RT @') < 0)):
 
-        string = t.created_at
-        string = string.strftime('%x,%r')
-        string = string + ',' + str(t.favorite_count) + ',' + str(t.retweet_count)
-        handle = {key['id_str']: key['screen_name'] for key in t.entities['user_mentions']}
-        for screen_name in handle.values():
-            string = string + ',' + screen_name
-        string = string + '\n'
-        out.write(string)
+            string = t.created_at
+            string = string.strftime('%x,%r')
+            string = string + ',' + str(t.favorite_count) + ',' + str(t.retweet_count)
+            handle = {key['id_str']: key['screen_name'] for key in t.entities['user_mentions']}
+            for screen_name in handle.values():
+                string = string + ',' + screen_name
+            string = string + '\n'
+            out.write(string)
+except Exception as error:
+    print(__name__, error)
 out.close()
