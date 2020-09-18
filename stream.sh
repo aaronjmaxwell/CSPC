b81fd53e4ca009d315153d541c80edc7933f5d7b
Aaron J. Maxwell, PhD - I only answer queries on the fifth Wednesday of each month <ajmax.85@gmail.com>
2020-09-18 16:04:51 -0400

    up-to-date.

--- stream.sh
+++ stream.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-file=tweet_activity_metrics_sciencepolicy_20200828_20200904_en.csv
+file=tweet_activity_metrics_sciencepolicy_20200911_20200918_en.csv
 
 echo "hashtag"
 python3 code/hashtag.py
